57a8d0d57117bfed3f678f1fd3af904b0ee43b70
nanshenghuan <775415606@qq.com>
2018-09-14 09:40:40 +0800

    ExplantationManager

--- TrainingPart/Assets/LeapMotionTutorial/Script/ExplanationMenuManager.cs
+++ TrainingPart/Assets/LeapMotionTutorial/Script/ExplanationMenuManager.cs
@@ -2,19 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class theNextOne : MonoBehaviour {
+public class ExplanationMenuManager : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        //showthmenu();
-    }
+        HideThisMenu();
+	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
-    public void showthmenu()
+    //隐藏当前的板子
+    public void HideThisMenu()
+    {
+        gameObject.SetActive(false);
+    }
+
+    public void ShowNextMenu()
     {
         gameObject.SetActive(true);
     }
+
 }
