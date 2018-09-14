f9d13d789a9410e9be5e0de3d24a6cf463c974ea
nanshenghuan <775415606@qq.com>
2018-09-14 09:08:53 +0800

    hideTheMenu

---
+++ TrainingPart/Assets/LeapMotionTutorial/Script/hideTheMenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hideTheMenu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        hidethemenu();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void hidethemenu()
+    {
+        gameObject.SetActive(false);
+    }
+}
