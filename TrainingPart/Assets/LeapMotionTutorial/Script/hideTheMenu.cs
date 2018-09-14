ce6a5f74171e05125fe6988a0157616ae20d4f73
nanshenghuan <775415606@qq.com>
2018-09-14 09:46:37 +0800

    EXxplantation2

--- TrainingPart/Assets/LeapMotionTutorial/Script/hideTheMenu.cs
+++ TrainingPart/Assets/LeapMotionTutorial/Script/hideTheMenu.cs
@@ -6,7 +6,7 @@ public class hideTheMenu : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        hidethemenu();
+        //hidethemenu();
     }
 	
 	// Update is called once per frame
