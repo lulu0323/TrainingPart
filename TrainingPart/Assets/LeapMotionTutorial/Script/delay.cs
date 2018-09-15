16f9f9a45a0c8a6a27a27e7c2bd70efee0d7ef45
pann5 <pann5@mail2.sysu.edu.cn>
2018-09-15 21:05:02 +0800

    delayShowTheMenu
    succeed3，4

---
+++ TrainingPart/Assets/LeapMotionTutorial/Script/delay.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//开始状态：板子被隐藏
+//隐藏板子
+public class delay : MonoBehaviour
+{
+
+
+    // Use this for initialization
+    void Start()
+    {
+        hidethemenu();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void showthmenu()
+    {
+        gameObject.SetActive(true);
+        print(Time.time);
+    }
+    public void hidethemenu()
+    {
+
+        gameObject.SetActive(false);
+    }
+
+
+    public void delayshowthemenu()
+    {
+
+        print(Time.time);
+        Invoke("showthmenu", 2);
+        print(Time.time);
+
+
+        Debug.Log("invokeTest");
+
+
+
+    }
+}
+
